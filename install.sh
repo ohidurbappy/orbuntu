5c2d239f361871d6e16f2c562a3e856f53ee414e
ohidurbappy <ohidurbappy@gmail.com>
2021-12-14 18:29:12 +0600

    fixed

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 # ********** Configurations **********
 OS_FLAVOR="orbuntu"
-USER="$(whoami)"
-
+# USER="$(whoami)"
 GTK_THEME="Mc-OS-CTLina-Gnome-Dark-1.3.2"
 SHELL_THEME="Sweet-Dark-v40"
 WALLPAPER="macos-wp.jpg"
@@ -12,22 +11,61 @@ ICON_THEME="Papirus"
 # *********** END Configurations ***********
 
 
+homedir=$( getent passwd "$USER" | cut -d: -f6 )
+username=$( getent passwd "$USER" | cut -d: -f1 )
+
+RUID=$(who | awk 'FNR == 1 {print $1}')
+RUSER_UID=$(id -u ${RUID})
+
+#Check if root
+if [[ "$EUID" -ne 0 ]]; then
+  isroot="false"
+	theme_install_dir="$homedir/.local/share/themes"
+	icon_install_dir="$homedir/.local/share/icons"
+    wallpaper_install_dir="$homedir/.local/share/backgrounds"
+	
+	sleep 0.5
+else
+  isroot="true"
+	homedir=$( getent passwd "$SUDO_USER" | cut -d: -f6 )
+	theme_install_dir="/usr/share/themes"
+	icon_install_dir="/usr/share/icons"
+    wallpaper_install_dir="/usr/share/backgrounds"
+fi
+
+echo -e "Theme Dir: $theme_install_dir"
+echo -e "Icon Dir: $icon_install_dir"
+echo -e "Wallpaper Dir: $wallpaper_install_dir"
+
+wallpaper_file="file:///$wallpaper_install_dir/$WALLPAPER"
+
+# create a temp dir
+# tmpdir=$(mktemp -d)
+tmpdir="$homedir/orbuntu-tmp"
+
+if [ ! -d "$tmpdir" ]; then
+  mkdir -p "$tmpdir"
+fi
+
+
+zip_file_path="$tmpdir/orbuntu.zip"
+
 # ********** Download the files **********
 echo "Downloading the files..."
-wget https://github.com/ohidurbappy/orbuntu/archive/refs/heads/main.zip -O orbuntu.zip
-ZIP_OUTPUT_DIR="$HOME/$OS_FLAVOR"
-sudo unzip orbuntu.zip -d $ZIP_OUTPUT_DIR
-sudo yes | cp -r $ZIP_OUTPUT_DIR/orbuntu-main/* $ZIP_OUTPUT_DIR
-rm orbuntu.zip
+wget https://github.com/ohidurbappy/orbuntu/archive/refs/heads/main.zip -O "$zip_file_path" -q --show-progress
+ZIP_OUTPUT_DIR="$tmpdir"
+unzip $zip_file_path -d "$tmpdir"
+rm "$zip_file_path"
 # *********** END Download the files ***********
 
-
+ZIP_OUTPUT_DIR="$tmpdir/orbuntu-main"
 # change directory to the folder
-cd $ZIP_OUTPUT_DIR
+cd "$ZIP_OUTPUT_DIR"
 
 # include the helper functions from 'src/helpers.sh'
-source src/helpers.sh
+source ./src/helpers.sh
 
+clear
 
 # show welcome message
 cat 'src/welcome.txt'
@@ -44,14 +82,62 @@ else
     exit 1
 fi
 
-# change the color to white
-echo -e "\e[0m"
-
 
 # ******** INSTALLING REQUIRED PACKAGES ********
 echo "Installing packages..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install git gnome-shell-extensions chrome-gnome-shell gnome-tweaks -y
+
+
+
+
+# ******** INSTALL THEMES, ICONS, WALLPAPER ********
+
+OUTPUT_DIRS=(
+    "$theme_install_dir"
+    "$icon_install_dir"
+    "$wallpaper_install_dir"
+)
+
+# create dirs if not exist
+for dir_name in "${OUTPUT_DIRS[@]}"
+do
+    if [ ! -d "$dir_name" ]; then
+        echo -e "$dir_name doesn't exist, creating now"
+        mkdir -p "$dir_name"
+    fi
+done
+
+theme_src_dir="$ZIP_OUTPUT_DIR/themes"
+icon_src_dir="$ZIP_OUTPUT_DIR/icons"
+
+
+echo "Unzipping the files..."
+
+for file in "$theme_src_dir"/*
+
+    do
+        tar -xvf "$file" -d "$theme_install_dir"
+    done
+
+# tar -xvf "$theme_src_dir"/* -C "$theme_install_dir"
+tar xvf "$icon_src_dir"/* -C "$icon_install_dir"
+mv "$ZIP_OUTPUT_DIR"/backgrounds/* "$wallpaper_install_dir"
+
+# ********** GNOME TWEAK SETTINGS ************
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
+gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
+gsettings set org.gnome.desktop.wm.preferences theme "$SHELL_THEME"
+gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
+# gsettings set org.gnome.desktop.interface font-name "Ubuntu Mono 11"
+# gsettings set org.gnome.mutter center-new-windows 'true'
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface clock-show-date 'true'
+gsettings set org.gnome.desktop.interface clock-show-weekday 'true'
+gsettings set org.gnome.desktop.background picture-uri "$wallpaper_file"
+
+
+exit 1
 
 
 # ******** FIREFOX UNINSTALL ********
@@ -64,10 +150,10 @@ sudo rm -rf /usr/lib/firefox
 
 
 # ******** UNINSTALL THUNDERBIRD MAIL ********
-# sudo apt-get purge thunderbird -y
-# sudo rm -rf ~/.thunderbird
-# sudo rm -rf /etc/thunderbird
-# sudo rm -rf /usr/lib/thunderbird
+sudo apt-get purge thunderbird -y
+sudo rm -rf ~/.thunderbird
+sudo rm -rf /etc/thunderbird
+sudo rm -rf /usr/lib/thunderbird
 # ******** UNINSTALL THUNDERBIRD MAIL ********
 
 
@@ -89,41 +175,6 @@ sudo apt install brave-browser
 
 
 
-# ******** INSTALL THEMES, ICONS, WALLPAPER ********
-
-OUTPUT_DIRS=(
-    "$HOME/.themes"
-    "$HOME/.icons"
-)
-
-# create dirs if not exist
-for dir_name in "${OUTPUT_DIRS[@]}"
-do
-    if [ ! -d "$dir_name" ]; then
-        mkdir "$dir_name"
-    fi
-done
-
-
-echo "Unzipping the files..."
-tar -xvf themes/* -C ~/.themes
-tar -xvf icons/* -C ~/.icons
-
-mv "$HOME/$OS_FLAVOR/backgrounds"/* /usr/share/backgrounds/
-
-echo_gray "Installing the themes..."
-
-# ********** GNOME TWEAK SETTINGS ************
-gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
-gsettings set org.gnome.desktop.interface gtk-theme "$GTK_THEME"
-gsettings set org.gnome.desktop.wm.preferences theme "$SHELL_THEME"
-gsettings set org.gnome.desktop.interface icon-theme "$ICON_THEME"
-# gsettings set org.gnome.desktop.interface font-name "Ubuntu Mono 11"
-# gsettings set org.gnome.mutter center-new-windows 'true'
-gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
-gsettings set org.gnome.desktop.interface clock-show-date 'true'
-gsettings set org.gnome.desktop.interface clock-show-weekday 'true'
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/$WALLPAPER"
 
 
 
@@ -134,14 +185,20 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 # ********** END CHANGE SYSTEM SETTINGS ************
 
 
+
 # ********** CONFIGURE GIT ************
 
-# ASK FOR USERNAME AND EMAIL
-echo_gray "Configuring git..."
-read -p "Enter your username: " USERNAME
-read -p "Enter your email: " EMAIL
-git config --global user.name "$USERNAME"
-git config --global user.email "$EMAIL"
+read -p "Do you want to configure git? (y/n): "
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # ASK FOR USERNAME AND EMAIL
+    echo_gray "Configuring git..."
+    read -p "Enter your username: " USERNAME
+    read -p "Enter your email: " EMAIL
+    git config --global user.name "$USERNAME"
+    git config --global user.email "$EMAIL"
+else
+    echo_red "Git configuration aborted!"
+fi
 
 # ********** END CONFIGURE GIT ************
 
